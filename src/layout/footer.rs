f9aa4235d12a81a8fff68709acebc27965d0d9aa
usagrada <usagrada.kind@gmail.com>
2021-10-04 17:21:59 +0900

    fix #3

--- src/layout/footer.rs
+++ src/layout/footer.rs
@@ -2,29 +2,21 @@ use yew::prelude::*;
 
 pub struct Footer {
     // props: Props,
-    _link: ComponentLink<Self>,
 }
 
 impl Component for Footer {
     type Message = ();
     type Properties = ();
 
-    fn create(_props: Self::Properties, _link: ComponentLink<Self>) -> Self {
-        Self { _link }
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {}
     }
 
-    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
-        false
-    }
+    fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
+		false
+	}
 
-    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-        // Should only return "true" if new properties are different to
-        // previously received properties.
-        // This component has no properties so we will always return "false".
-        false
-    }
-
-    fn view(&self) -> Html {
+	fn view(&self, _ctx: &Context<Self>) -> Html {
         html! {
             <div>
                 {"footer"}
